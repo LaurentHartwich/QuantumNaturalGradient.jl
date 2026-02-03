6d9fb07be4f3217dfad6617f926374a53e867afb
Laurent Hartwich <hartwich@iff1702>
2026-02-03 16:07:32 +0100

    add some docstrings to solver related functions

--- src/solver/solver.jl
+++ src/solver/solver.jl
@@ -1,22 +1,10 @@
 abstract type AbstractSolver end
 
-function solve_S(solver::AbstractSolver, J::Jacobian, grad_half::Vector; timer=TimerOutput(), kwargs...)
-    @timeit "dense_S" Jd = dense_S(J)
-    @timeit "solve" θdot = -solver(Jd, grad_half; kwargs...)
-    
-    return θdot
-end
+"""
+(solver::AbstractSolver)(ng::NaturalGradient; method=:auto, compute_error=true, kwargs...)
 
-function solve_T(solver::AbstractSolver, J::Jacobian, Es::EnergySummary; timer=TimerOutput(), kwargs...)
-    @timeit "dense_T" Jd = dense_T(J)
-    Ekms = centered(Es)
-
-    @timeit "solve" θdot_raw = -solver(Jd, Ekms; kwargs...)
-    θdot = centered(J)' * θdot_raw
-
-    return θdot
-end
-
+Computes θdot corresponding to equations (11) or (13) of https://arxiv.org/pdf/2503.12557, depending on the method (solve_T corresponds to eq. (13), while solve_S corresponds to eq. (11)).
+"""
 function (solver::AbstractSolver)(ng::NaturalGradient; method=:auto, compute_error=true, kwargs...)
     if method === :T || (method === :auto && nr_samples(ng.J) < nr_parameters(ng.J))
         ng.θdot = solve_T(solver, ng.J, ng.Es; kwargs...)
@@ -29,6 +17,32 @@ function (solver::AbstractSolver)(ng::NaturalGradient; method=:auto, compute_err
     return ng
 end
 
+"""
+solve_S(solver::AbstractSolver, J::Jacobian, grad_half::Vector; timer=TimerOutput(), kwargs...)
+
+In the terms of https://arxiv.org/pdf/2503.12557, this returns θdot = (O' * O)^-1 * O' * E_loc   (Eq. 11)
+"""
+function solve_S(solver::AbstractSolver, J::Jacobian, grad_half::Vector; timer=TimerOutput(), kwargs...)
+    @timeit "dense_S" Jd = dense_S(J)
+    @timeit "solve" θdot = -solver(Jd, grad_half; kwargs...)
+    
+    return θdot
+end
+
+"""
+solve_T(solver::AbstractSolver, J::Jacobian, Es::EnergySummary; timer=TimerOutput(), kwargs...)
+
+In the terms of https://arxiv.org/pdf/2503.12557, this returns θdot = O' * (O * O')^-1 * E_loc   (Eq. 13)
+"""
+function solve_T(solver::AbstractSolver, J::Jacobian, Es::EnergySummary; timer=TimerOutput(), kwargs...)
+    @timeit "dense_T" Jd = dense_T(J)
+    Ekms = centered(Es)
+
+    @timeit "solve" θdot_raw = -solver(Jd, Ekms; kwargs...)
+    θdot = centered(J)' * θdot_raw
+
+    return θdot
+end
 
 function (solver::AbstractSolver)(M::AbstractMatrix, v::AbstractArray, double::Bool; method=:auto, kwargs...)
     if double
